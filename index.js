474236558022566a34621b5d9986acd02182d1dd
fippli <filip.carl.johansson@gmail.com>
2018-09-10 15:38:31 -0500

    Added filter for data and updated input praameters

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ Express utils
 
 // RESPONSES
 
-const errorResponse = ( message, error, response ) => {
+const errorResponse = ( message, response ) => {
 
-    return () => {
+    return error => {
         console.log( message, error );
         response.status( 500 ).send({message: message});
     };
 
 };
 
-const successResponse = ( message, data, response ) => {
+const successResponse = ( message, response, dataFilter = data => data ) => {
 
-    return () => {
+    return data => {
         console.log( message, data );
-        response.status( 200 ).send({message: message, data: data});
+        response.status( 200 ).send({message: message, data: dataFilter(data)});
     }
 
 };
