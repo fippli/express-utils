3d74c1d4e412a01ef18946dc84cbeb95a5ec148f
fippli <filip.carl.johansson@gmail.com>
2018-09-10 15:53:53 -0500

    Added more response types

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorResponse = ( message, response ) => {
 
     return error => {
         console.log( message, error );
-        response.status( 500 ).send({message: message});
+        response.status( 500 ).send( { message: message } );
     };
 
 };
@@ -17,12 +17,58 @@ const successResponse = ( message, response, dataFilter = data => data ) => {
 
     return data => {
         console.log( message, data );
-        response.status( 200 ).send({message: message, data: dataFilter(data)});
+        response.status( 200 ).send( { message: message, data: dataFilter( data ) } );
     }
 
+};
+
+//
+// General responses
+//
+
+// Success
+const getSuccess = ( response, dataName, dataFilter ) => {
+    return successResponse( `Got ${dataName}.`, response, dataFilter );
+};
+
+const putSuccess = ( response, dataName, dataFilter ) => {
+    return successResponse( `${dataName} was successfully updated.`, response, dataFilter );
+};
+
+const postSuccess = ( response, dataName, dataFilter ) => {
+    return successResponse( `${dataName} was successfully added.`, response, dataFilter );
+};
+
+const deleteSuccess = ( response, dataName, dataFilter ) => {
+    return successResponse( `${dataName} was successfully deleted.`, response, dataFilter );
+};
+
+// Error
+const getError = () => {
+    return errorResponse( `Could not get ${dataName}.`, response );
+};
+
+const putError = () => {
+    return errorResponse( `Could not update ${dataName}.`, response );
+};
+
+const postError = () => {
+    return errorResponse( `Could not add ${dataName}.`, response );
+};
+
+const deleteError = () => {
+    return errorResponse( `Could not delete ${dataName}.`, response );
 };
 
 module.exports = {
     errorResponse: errorResponse,
     successResponse: successResponse,
+    getSuccess: getSuccess,
+    putSuccess: putSuccess,
+    postSuccess: postSuccess,
+    deleteSuccess: deleteSuccess,
+    getError: getError,
+    putError: putError,
+    postError: postError,
+    deleteError: deleteError,
 };
